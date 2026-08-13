783154a09fe9b4608ae915f4fdc05adc132b7277
lighting8282 <lighting8282@users.noreply.github.com>
2026-08-13 11:18:52 -0400

    Revert "Remove the Prof. Oak's Check panel"
    This reverts commit ab4025e18ff3a2cda4a247227a658c35067a5efc.

--- scripts/init.lua
+++ scripts/init.lua
@@ -76,6 +76,7 @@ Tracker:AddLocations("locations/volcano.json")
 Tracker:AddLocations("locations/river.json")
 Tracker:AddLocations("locations/cave.json")
 Tracker:AddLocations("locations/valley.json")
+Tracker:AddLocations("locations/oakmew.json")
 
 ScriptHost:LoadScript("scripts/course_items.lua")
 
